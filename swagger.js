79f3ff31e6989f8039e7ae97f55787236132aeb4
...Pau <>
2024-07-31 10:19:26 -0500

    update

--- swagger.js
+++ swagger.js
@@ -5,6 +5,7 @@ const swaggerAutogen = require('swagger-autogen')()
      "title": 'REST API',
      "description": 'REST API with Express and Firestore'
    },
+   "host": 'localhost:5500',
     "host": 'restapi-czz7.onrender.com',
    "schemes": [
      "https"
